bccf1ba0071db61d9015bb5c7e3739f37fe6858b
LagSistem <140086830+LagSistem@users.noreply.github.com>
2023-07-20 17:13:37 +0300

    Admin-Raspis

---
+++ AdminScr.js
@@ -0,0 +1,10 @@
+function Open_modul() {
+    let menu = document.getElementsByClassName('model');
+    console.log(menu[0]);
+    menu[0].style.display = 'flex';
+}
+function Close_modul() {
+    let menu = document.getElementsByClassName('model');
+    console.log(menu[0]);
+    menu[0].style.display = 'none';
+}
